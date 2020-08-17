d6c858470562e10826541149b2af721246022f9e
Daniel Brenes <danbg31@gmail.com>
2020-08-17 01:15:59 -0600

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import { render } from "react-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Shop from "./Shop";
+import NavBar from "./Navbar";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} exact />
+        <Route path="/shop" component={Shop} exact />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   );
+
+  /*
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} exact />
+        <Route path="/contacts" component={Contacts} exact />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
+
+
+  );
+  */
 }
 
 export default App;
